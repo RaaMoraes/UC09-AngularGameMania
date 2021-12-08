cd669e74a965ae9a4cef43c2a24085c918893bee
RaaMoraes <raissa_m_castro@hotmail.com>
2021-12-07 21:13:31 -0300

    Versao do site Game Mania utilizando o Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContentComponent } from './content/content.component';
+import { HomeComponent } from './home/home.component';
+import { PaginaProdutoComponent } from './pagina-produto/pagina-produto.component';
+import { QuemsomosComponent } from './quemsomos/quemsomos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"cadastro", component:ContentComponent},
+  {path:"pagina-produto", component:PaginaProdutoComponent},
+  {path:"quem-somos", component:QuemsomosComponent},
+  {path:"", component:HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
+import { FormsModule } from '@angular/forms';
+import { PaginaProdutoComponent } from './pagina-produto/pagina-produto.component';
+import { QuemsomosComponent } from './quemsomos/quemsomos.component';
+import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastroComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContentComponent,
+    PaginaProdutoComponent,
+    QuemsomosComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
